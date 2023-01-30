e01d50afc3e067384f33cf0fee1247bdc12779be
af176 <90385401+af176@users.noreply.github.com>
2023-01-30 14:41:14 +0200

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,17 @@
+
+orders = [{"id": 1, "dough_time": 7, "toppings": [{"id": 1, "time": 4}, {"id": 2, "time": 4}, ],
+           "cook_time": 10,
+           "serve_time": 5,
+           },
+          {
+              "id": 2,
+              "dough_time": 7,
+              "toppings": [
+                  {"id": 3, "time": 4},
+                  {"id": 4, "time": 4},
+              ],
+              "cook_time": 10,
+              "serve_time": 5,
+          }
+          ]
+
